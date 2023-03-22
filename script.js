690d2345244d56f717c4b4754c0380d5b27b6eea
Sazzad Hossain <83215640+sazzadhossain1623@users.noreply.github.com>
2023-03-22 22:52:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -22,12 +22,6 @@ const updateTime = () => {
     minToDeg = (date.getMinutes() / 60) * 360,
     hrToDeg = (date.getHours() / 12) * 360;
 
-
-
-
-
-    
-
   secondHand.style.transform = `rotate(${secToDeg}deg)`;
   minuteHand.style.transform = `rotate(${minToDeg}deg)`;
   hourHand.style.transform = `rotate(${hrToDeg}deg)`;
@@ -36,3 +30,47 @@ const updateTime = () => {
 setInterval(updateTime, 1000);
 
 updateTime();
+
+setInterval(showTime, 1000);
+function showTime() {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var date = new Date();
+  var monthName = months[date.getMonth()];
+
+  var days = new Array(7);
+  days[0] = "Sunday";
+  days[1] = "Monday";
+  days[2] = "Tuesday";
+  days[3] = "Wednesday";
+  days[4] = "Thursday";
+  days[5] = "Friday";
+  days[6] = "Saturday";
+  var day = days[date.getDay()];
+
+  am_pm = "AM";
+
+  if (hour > 12) {
+    hour -= 12;
+    am_pm = "PM";
+  }
+  if (hour == 0) {
+    hr = 12;
+    am_pm = "AM";
+  }
+
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  let currentTime = hour + ":"
+    + min + ":" + sec + " " + am_pm + ", " + monthName + " " + date.getDate() + "," + " " + date.getFullYear();
+
+  document.getElementById("show-time")
+    .innerHTML = currentTime;
+}
+
+showTime();
